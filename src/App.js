6e6a49e25a11c6c10cc28d62198a70915d739f47
ADITYA KUMAR SINGH <90272291+AD1TYA03@users.noreply.github.com>
2022-05-28 16:06:14 +0530

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./styles.css";
-
+import Header from "../src/components/header";
 export default function App() {
   return (
     <div className="App">
-   
+      <Header />
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles.css";
+function header() {
+  return (
+    <div className="headerMain">
+      <img src="https://i.ibb.co/fpgM7Qw/masterji.png" alt="logo"></img>
+      <h1 className="logoName">E MASTARJI</h1>
+      <div className="headButtons">
+        <ul>
+          <li>
+            <button>Home</button>
+          </li>
+          <li>
+            <button>Schedule</button>
+          </li>
+          <li>
+            <button>Live Classes</button>
+          </li>
+          <li>
+            <button>Notes</button>
+          </li>
+        </ul>
+      </div>
+      <button className="loginBtn">LOGIN</button>
+      <button className="loginBtn2">SIGN UP</button>
+    </div>
+  );
+}
+
+export default header;
