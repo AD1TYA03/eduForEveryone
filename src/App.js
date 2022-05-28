31da171f624262c6958c390c0c746cd7ea787b8d
ADITYA KUMAR SINGH <90272291+AD1TYA03@users.noreply.github.com>
2022-05-28 17:10:18 +0530

    intro added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
 import Header from "../src/components/header";
+import Intro from "../src/components/intro";
 export default function App() {
   return (
     <div className="App">
       <Header />
+      <Intro />
     </div>
   );
 }
---
+++ src/components/intro.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function intro() {
+  return (
+    <div className="introMain">
+      <div className="intro2">
+        <img src="https://i.ibb.co/rs8p0qN/education.png" alt="books pic"></img>
+
+        <button className="startBtn">START LEARNING</button>
+      </div>
+    </div>
+  );
+}
+
+export default intro;
