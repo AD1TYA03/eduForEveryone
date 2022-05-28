dac364e9ed5ad1f041d643354eb05227a15475df
ADITYA KUMAR SINGH <90272291+AD1TYA03@users.noreply.github.com>
2022-05-29 03:41:24 +0530

    sponsors added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from "../src/components/header";
 import Intro from "../src/components/intro";
 import About from "../src/components/about";
 import Schedule from "../src/components/schedule";
+import Sponsor from "../src/components/sponsor";
+import Contact from "../src/components/contact";
 export default function App() {
   return (
     <div className="App">
@@ -10,6 +12,8 @@ export default function App() {
       <Intro />
       <About />
       <Schedule />
+      <Sponsor />
+      <Contact />
     </div>
   );
 }
---
+++ src/components/sponsor.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+function sponsor() {
+  return (
+    <div>
+      <h2 className="toolsHeading">OUR SPONSORS</h2>
+      <div className="line"></div>
+      <div className="aboutText">
+        <p>
+          Our sponsor are the one who lights the candle of Education in the
+          Darkness due to corruption and poor Governance.
+        </p>
+      </div>
+      <div className="sponsorCards">
+        <div>
+          <img
+            src="https://images.livemint.com/img/2020/08/13/600x338/LocoPA_1592313175874_1592313196211_1597308383286.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/ITC_Limited_Logo.svg/1200px-ITC_Limited_Logo.svg.png"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://1000logos.net/wp-content/uploads/2021/09/HCL-Logo.png"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://indianroadsafety.s3.amazonaws.com/ngoteam_image/9036ef3375dc4cebae65a232b4935ebc.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://1000logos.net/wp-content/uploads/2017/03/Nestle-Logo-1984.jpg"
+            alt=""
+          ></img>
+        </div>
+        <div>
+          <img
+            src="https://lawprofessors.typepad.com/.a/6a00d8341bfae553ef01b8d1594773970c-800wi"
+            alt=""
+          ></img>
+        </div>
+        {/* <div>
+          <img
+            src="https://m.media-amazon.com/images/I/41YTcyA8eeL.jpg"
+            alt=""
+          ></img>
+        </div> */}
+        <div>
+          <img src="https://graphhene.org/images/logo.png" alt=""></img>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default sponsor;
